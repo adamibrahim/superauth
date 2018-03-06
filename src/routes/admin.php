ce3d475d16cba5a76c79fe745d1fa629d1ec7b77
adam_ibrahim <adamibrahim1701@gmail.com>
2018-03-06 14:18:31 +0500

    pushing test views

--- src/routes/admin.php
+++ src/routes/admin.php
@@ -6,7 +6,7 @@ Route::
     ->prefix(config('superauth.admin_prefix'))
     ->group(function() {
     Route::get('login', 'Adam\Superauth\Controllers\Admin\Auth\LoginController@showLoginForm')->name('login');
-    Route::get('test', 'Adam\Superauth\Controllers\Admin\TestDashboardController@index')->name('test.dashboard')->middleware('auth');
+    Route::get('test', 'Adam\Superauth\Controllers\Admin\TestDashboardController@index')->name('test.dashboard')->middleware('moderators');
     Route::post('test', 'Adam\Superauth\Controllers\Admin\TestDashboardController@updateRoles')->name('roles.update')->middleware('auth');
 
 });
